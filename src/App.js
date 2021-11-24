6e8043495bc5fe86a2bf3e6ce2f40056b392d589
Avrahm <avrahmk@me.com>
2021-11-24 00:33:41 -0500

    project 1 - section 2 - frontend update

--- src/App.js
+++ src/App.js
@@ -5,19 +5,23 @@ import './App.css';
 export default function App() {
 
   const wave = () => {
-    
+
   }
-  
+
   return (
     <div className="mainContainer">
 
       <div className="dataContainer">
         <div className="header">
-        👋 Hey there!
+          👋 Welcome to Avrahm's Wave Portal!
         </div>
 
         <div className="bio">
-        I am farza and I worked on self-driving cars so that's pretty cool right? Connect your Ethereum wallet and wave at me!
+          I'm Avrahm, a father, skydiver, web developer, and now a blockchain developer!
+          <br />
+          <br />
+          This is my Wave Portal, a decentralized application that allows users to create and manage their own waves.
+          <p>Connect your Ethereum wallet and wave at me! </p>
         </div>
 
         <button className="waveButton" onClick={wave}>
